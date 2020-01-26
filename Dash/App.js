cf244cb1726afcd6132f1b7051173c81eb9b955e
Gautam <gautamgupta9298@gmail.com>
2020-01-26 05:10:21 -0500

    Push not added

--- Dash/App.js
+++ Dash/App.js
@@ -6,15 +6,19 @@
  * @flow
  */
 
-import React from 'react';
+import React, { Component, useState, useEffect } from 'react';
+
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
+  Image,
   StatusBar,
-  Dimensions
+  Dimensions,
+  Animated,
+  Easing
 } from 'react-native';
 
 import {
@@ -51,9 +55,22 @@ import Spinner from './SpinnerComponent';
 
 import Firebase from 'firebase';
 
+var PushNotification = require("react-native-push-notification");
+
+
+import styled from 'styled-components'
+const keyframes = require('styled-components').keyframes
+
+
 var firebaseConfig = {
-  databaseURL: "<database-url>",
-  projectId: "<project-id>",
+  apiKey: "AIzaSyAzWMeNwyE8OH4L7BLOS6e2DqkraRZMnlo",
+  authDomain: "esp32test-acf49.firebaseapp.com",
+  databaseURL: "https://esp32test-acf49.firebaseio.com",
+  projectId: "esp32test-acf49",
+  storageBucket: "esp32test-acf49.appspot.com",
+  messagingSenderId: "870795617355",
+  appId: "1:870795617355:web:5aa3c606c47f56724799ce",
+  measurementId: "G-CZJG22MZQ8"
 };
 // you can set your style right here, it'll be propagated to application
 const uiTheme = {
@@ -82,108 +99,229 @@ const chartConfig = {
   barPercentage: 0.5
 };
 
-const dataPro = {
-  data: [0.4]
-}
-const dataPro2 = {
-  data: [0.8]
-}
+const FadeInView = (props) => {
+  const [fadeAnim] = useState(new Animated.Value(0))  // Initial value for opacity: 0
 
-
-const App: () => React$Node = () => {
-
-  Firebase.initializeApp(firebaseConfig);
+  React.useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 1000,
+      },
+    ).start();
+  }, [])
 
   return (
-    <>
-      {
-        <ThemeContext.Provider value={getTheme(uiTheme)}>
-          <View>
-            <Toolbar
-              key="toolbar"
-              centerElement="SMART DASH"
-            />
-            <LinearGradient
-              colors={['#0d47a1', '#0d47a1']}
-              style={{ padding: 15, alignItems: 'center', height: '100%' }}>
-              <View>
-                <Text style={styles.subTitles}>Daily Trend</Text>
-                <LineChart
-                  data={{
-                    labels: ["January", "February", "March", "April", "May", "June"],
-                    datasets: [
-                      {
-                        data: [
-                          Math.random() * 100,
-                          Math.random() * 100,
-                          Math.random() * 100,
-                          Math.random() * 100,
-                          Math.random() * 100,
-                          Math.random() * 100
-                        ]
-                      }
-                    ]
-                  }}
-                  width={(Dimensions.get("window").width) * 0.95} // from react-native
-                  height={220}
-                  yAxisLabel="$"
-                  yAxisSuffix="k"
-                  chartConfig={{
-                    decimalPlaces: 2, // optional, defaults to 2dp
-                    backgroundGradientFrom: "#ffffff",
-                    backgroundGradientFromOpacity: 1,
-                    backgroundGradientTo: "#ffffff",
-                    backgroundGradientToOpacity: 1,
-                    fillShadowGradient: '#5471d2',
-                    fillShadowGradientOpacity: 0.2,
-                    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-                    labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-                    style: {
-                      borderRadius: 16
-                    },
-                    propsForDots: {
-                      r: "6",
-                      strokeWidth: "2",
-                      stroke: "#0d47a1"
-                    }
-                  }}
-                  bezier
-                  style={styles.floatView}
-                />
-              </View>
-              <View style={{ flexDirection: 'row', width: '100%', justifyContent: 'space-between', paddingTop: 24 }}>
-                <View style={{ textAlign: 'center' }}>
-                  <Text style={styles.subTitles2}>Wrist</Text>
-                  <ProgressChart
-                    data={dataPro}
-                    width={(Dimensions.get("window").width) * 0.33} // from react-native
-                    height={100}
-                    chartConfig={chartConfig}
-                    hideLegend={true}
-                    style={{ paddingLeft: 12 }}
-                  />
-                </View>
-                <View style={{ textAlign: 'center' }}>
-                  <Text style={styles.subTitles2}>Body</Text>
-                  <ProgressChart
-                    data={dataPro2}
-                    width={(Dimensions.get("window").width) * 0.33} // from react-native
-                    height={100}
-                    chartConfig={chartConfig}
-                    hideLegend={true}
-                    style={{ paddingLeft: 12 }}
-                  />
-                </View>
-              </View>
-              <View style={{paddingTop: 30}}>
-                <Spinner></Spinner>
-              </View>
-            </LinearGradient>
-          </View>
-        </ThemeContext.Provider>
-      }
-    </>
+    <Animated.Image                 // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim,
+        width: 100      // Bind opacity to animated value
+      }}
+      source={require('./assets/hand.png')}
+    >
+      {props.children}
+    </Animated.Image>
   );
+}
+
+export default class HelloWorldApp extends Component {
+
+  state = {
+    wristData: { data: [0] },
+    bodyData: { data: [0] },
+    angleData: { data: [0] },
+    labelData: ["1 min"],
+    spinAngle: 0,
+    lastSpinAngle: 0
+  }
+
+  readBodyData() {
+    Firebase.database().ref('/').on('value', (snapshot) => {
+      this.setState({
+        bodyData: { data: [(snapshot.val().body / 10)] }
+      })
+      console.log(this.state['bodyData']);
+    }, function (error) {
+      console.log(error);
+    });
+  }
+  readWristData() {
+    Firebase.database().ref('/').on('value', (snapshot) => {
+      this.setState({
+        wristData: { data: [(snapshot.val().wrist / 10)] }
+      })
+      console.log(this.state['wristData']);
+    }, function (error) {
+      console.log(error);
+    });
+  }
+
+  readAngleData() {
+    Firebase.database().ref('GyroReadings/').on('value', (snapshot) => {
+      console.log(snapshot.val());
+      var lengthArr = snapshot.val().length;
+      var numEntries = Math.ceil(lengthArr / 60);
+      if (numEntries == 1) {
+        this.setState({
+          labelData: ["0 min", "1 min"]
+        })
+      }
+      else if (numEntries == 2) {
+        this.setState({
+          labelData: ["0 min", "1 min", "2 min"]
+        })
+      }
+      else if (numEntries == 3) {
+        this.setState({
+          labelData: ["0 min", "1 min", "2 min", "3 min"]
+        })
+      }
+      else if (numEntries == 4) {
+        this.setState({
+          labelData: ["0 min", "1 min", "2 min", "3 min", "4 min"]
+        })
+      }
+      else if (numEntries == 5) {
+        this.setState({
+          labelData: ["0 min", "1 min", "2 min", "3 min", "4 min", "5 min"]
+        })
+      }
+      this.setState({
+        angleData: { data: (snapshot.val()) }
+      })
+      this.setState({
+        spinAngle: snapshot.val()[snapshot.val().length - 1]
+      })
+    }, function (error) {
+      console.log(error);
+    });
+  }
+
+  readVibration() {
+    Firebase.database().ref('/').on('value', (snapshot) => {
+      if (snapshot.val().vibration == 1) {
+        PushNotification.localNotification({
+          vibrate: true, // (optional) default: true
+          vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
+
+
+            /* iOS and Android properties */
+          title: "Smart Brace: Alert", // (optional)
+          message: "Poor wrist angles detected. Please correct your posture", // (required)
+          playSound: false, // (optional) default: true
+          soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
+          actions: '["Yes", "No"]',  // (Android only) See the doc for notification actions to know more
+        });
+      }
+    }, function (error) {
+      console.log(error);
+    });
+  }
+
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+    if (!Firebase.apps.length) {
+      Firebase.initializeApp(firebaseConfig);
+    }
+    this.readBodyData();
+    this.readWristData();
+    this.readAngleData();
+    this.readVibration();
+  }
+
+  render() {
+    return (
+      <>
+        {
+          <ThemeContext.Provider value={getTheme(uiTheme)}>
+            <View>
+              <Toolbar
+                key="toolbar"
+                centerElement="SMART DASH"
+              />
+              <LinearGradient
+                colors={['#0d47a1', '#0d47a1']}
+                style={{ padding: 15, alignItems: 'center', height: '100%' }}>
+                <View>
+                  <Text style={styles.subTitles}>Daily Trend</Text>
+                  <LineChart
+                    data={{
+                      labels: this.state['labelData'],
+                      datasets: [
+                        this.state['angleData']
+                      ]
+                    }}
+                    width={(Dimensions.get("window").width) * 0.95} // from react-native
+                    height={300}
+                    chartConfig={{
+                      decimalPlaces: 0, // optional, defaults to 2dp
+                      backgroundGradientFrom: "#ffffff",
+                      backgroundGradientFromOpacity: 1,
+                      backgroundGradientTo: "#ffffff",
+                      backgroundGradientToOpacity: 1,
+                      fillShadowGradient: '#5471d2',
+                      fillShadowGradientOpacity: 0.2,
+                      color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                      labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                      style: {
+                        borderRadius: 16
+                      },
+                      propsForDots: {
+                        r: "3",
+                        strokeWidth: "1",
+                        stroke: "#0d47a1"
+                      }
+                    }}
+                    bezier
+                    style={styles.floatView}
+                  />
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                  <View style={{ flexDirection: 'column', width: '100%', justifyContent: 'space-between', paddingTop: 24 }}>
+                    <View style={{ textAlign: 'left' }}>
+                      <Text style={styles.subTitles2}>Wrist</Text>
+                      <ProgressChart
+                        data={this.state['wristData']}
+                        width={(Dimensions.get("window").width) * 0.33} // from react-native
+                        height={100}
+                        chartConfig={chartConfig}
+                        hideLegend={true}
+                        style={{ paddingLeft: 12 }}
+                      />
+                    </View>
+                    <View style={{ textAlign: 'left' }}>
+                      <Text style={styles.subTitles2}>Body</Text>
+                      <ProgressChart
+                        data={this.state['bodyData']}
+                        width={(Dimensions.get("window").width) * 0.33} // from react-native
+                        height={100}
+                        chartConfig={chartConfig}
+                        hideLegend={true}
+                        style={{ paddingLeft: 12 }}
+                      />
+                    </View>
+                  </View>
+                </View>
+                <View style={{ flexDirection: 'row', alignItems: 'flex-end', zIndex: 5, position: 'absolute', top: '50%', left: '40%' }}>
+                  <Image style={{ width: 300 }} source={require('./assets/arm.png')} />
+                </View>
+                <View style={{ flexDirection: 'row', alignItems: 'flex-end', zIndex: 10, position: 'absolute', top: '60.5%', left: '56.5%' }}>
+                  <FadeInView></FadeInView>
+                  {/* <Animated.Image style={{ width: 100}} source={require('./assets/hand.png')} /> */}
+                </View>
+                <View style={{ paddingTop: 35 }}>
+                  <Spinner></Spinner>
+                </View>
+              </LinearGradient>
+            </View>
+          </ThemeContext.Provider>
+        }
+      </>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -232,18 +370,16 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginRight: 5,
     marginTop: 10,
-    borderRadius: 20
+    borderRadius: 20,
   },
   subTitles: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 30,
     paddingLeft: 8,
   },
   subTitles2: {
-    alignSelf: 'center',
+    alignSelf: 'flex-start',
     color: 'white',
-    fontSize: 20,
+    fontSize: 30,
   }
 });
-
-export default App;
